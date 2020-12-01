58ed0df5caeeeb2de711339120e59df375700547
hilbigan <darkolan2@gmail.com>
2020-12-01 16:23:35 +0100

    Add HTTP Archive queries

---
+++ collection/web/httparchive/README.md
@@ -0,0 +1,3 @@
+The sql files above contain the queries we used to collect URLs from
+Google BigQuery. We also provide the resulting csv files which we
+directly downloaded binaries from.
---
+++ collection/web/httparchive/q1.sql
@@ -0,0 +1,2 @@
+SELECT DISTINCT(url) FROM `httparchive.summary_requests.2020_03_01_desktop` 
+WHERE (mimeType = 'application/wasm' OR mimeType = 'application/octet-stream' OR mimeType = "application/wasm;charset=UTF-8") AND url LIKE "%.wasm%"
